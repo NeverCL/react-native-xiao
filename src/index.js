98abe28e1f02c74b3de85d3a78929bbdc911aff4
Never、C <caolin>
2017-01-19 17:23:57 +0800

    fix index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Button, View } from 'antd-mobile';
+import { Alert } from 'react-native';
 
 import {
     isFirstTime,
@@ -22,9 +23,18 @@ class App extends Component {
             <View>
                 <Button onClick={() => {
                     checkUpdate(appKey).then(info => {
-                        downloadUpdate(info).then(hash => {
-                            switchVersion(hash);
-                        });
+                        if (info.upToDate) {
+                            Alert.alert('提示', '您的应用版本已是最新.');
+                        } else {
+                            Alert.alert('提示', '检查到新的版本' + info.name + ',是否立即更新?\n' + info.description, [
+                                {
+                                    text: '是', onPress: () => downloadUpdate(info).then(hash =>
+                                        switchVersion(hash)
+                                    )
+                                },
+                                { text: '否', },
+                            ]);
+                        }
                     }).catch(err => {
                         Alert.alert('提示', '更新失败.');
                     });
