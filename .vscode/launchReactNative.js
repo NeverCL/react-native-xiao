42224c917e44ff7c801b46fd5a2402e31c66a14c
Never、C <caolin>
2017-01-26 14:28:41 +0800

    fix fn

--- .vscode/launchReactNative.js
+++ .vscode/launchReactNative.js
@@ -2,8 +2,8 @@
 // Please do not modify it manually. All changes will be lost.
 try {
     var path = require("path");
-    var Launcher = require("C:\\Users\\CL\\.vscode\\extensions\\vsmobile.vscode-react-native-0.2.5\\out\\debugger\\launcher.js").Launcher;
-    new Launcher("e:\\Temp\\rn\\Xiao", "e:\\Temp\\rn\\Xiao").launch();
+    var Launcher = require("C:\\Users\\35761\\.vscode\\extensions\\vsmobile.vscode-react-native-0.2.5\\out\\debugger\\launcher.js").Launcher;
+    new Launcher("c:\\Code\\GitHub\\react-native-xiao", "c:\\Code\\GitHub\\react-native-xiao").launch();
 } catch (e) {
     throw new Error("Unable to launch application. Try deleting .vscode/launchReactNative.js and restarting vscode.");
 }
